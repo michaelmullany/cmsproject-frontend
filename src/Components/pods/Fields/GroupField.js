faca558822c8cff9222627542add6768ef428c27
Michael Mullany <mamullany@gmail.com>
2022-05-27 14:45:52 +0100

    Group field readonly to disabled

--- src/Components/pods/Fields/GroupField.js
+++ src/Components/pods/Fields/GroupField.js
@@ -2,7 +2,7 @@ export const GroupField = ({ groupName}) => {
     return (
         <div className="inputGroup inputGroupLine">
             <label htmlFor="selectGroup">Group: </label>
-            <input type="text" id="selectGroup" name="selectGroup" value={groupName} readOnly />
+            <input type="text" id="selectGroup" name="selectGroup" value={groupName} disabled />
         </div>     
     )
 }
