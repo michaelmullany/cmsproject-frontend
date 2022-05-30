eb215884e251df94c692b19e5f26cd158d931b8e
Ian Connell <ian10gt@gmail.com>
2022-05-30 11:15:30 +0100

    regex added along with search results reload on input

--- src/Components/pods/ManageGroupsPod.js
+++ src/Components/pods/ManageGroupsPod.js
@@ -4,18 +4,32 @@ import { postComponents } from "../../utils";
 
 export const ManageGroupsPod = () => {
     const [existingComponents, setExistingComponents] = useState([]);
+    const [searchField, setSearchField] = useState("");
     
     
     useEffect(() => {
-        postComponents("testN", setExistingComponents);
-    }, []);
+        postComponents(searchField, setExistingComponents);
+    }, [existingComponents]);
+
         
+
+    const searchSubmitHandler = (e) => {
+         {
+            e.preventDefault();
+        }
+    }
   
     return (
         <div className="pod fullPod tablePod">
             <div className="halfPodHeader">
             <h2>Components</h2>
             </div>
+
+            <form onSubmit={searchSubmitHandler}>
+                <label>Search </label>                
+                <input placeholder="Search components" onKeyPress={searchSubmitHandler} onInput={(e) => {setSearchField(e.target.value)} }></input>
+            </form>
+
             <div id="recentComponentsBodyContainer">
                 <div id="recentComponentsBody">
                     <div className="podTableContainer">
@@ -27,13 +41,13 @@ export const ManageGroupsPod = () => {
                                     <th>Modified</th>
                                 </tr>
                             </thead>
-                            <tbody>
-                               
+                            <tbody>     
+                                                          
                                 {existingComponents.map(x => {
                                     return (<tr>
                                             <td>{x.componentName}</td>
                                             <td>{x.component}</td>
-                                            <td>"01/01/2022"</td>
+                                            <td>01/01/2022</td>
                                             </tr>
                                     )
                                 })}                                
