f98f493dec71b76858b7d690865a6bbf8943f52f
Ian Connell <ian10gt@gmail.com>
2022-05-31 15:30:10 +0100

    edittextpod.js pod update to take up full page

--- src/Components/pods/Fields/GroupFieldExisting.js
+++ src/Components/pods/Fields/GroupFieldExisting.js
@@ -1,13 +1,21 @@
-export const GroupField = ({ existingGroups, setGroup }) => {
+import { useState } from 'react'
+
+export const GroupFieldExisting = ({ existingGroups, setGroup, editComponentObj }) => {
+
+
+
     return (
         <div className="inputGroup inputGroupLine">
             <label htmlFor="selectGroup">Group: </label>
-            <select id="selectGroup" name="selectGroup" size="1" onChange={(e) => setGroup(e.target.value)}>           
+            <select id="selectGroup" name="selectGroup" size="1" onChange={(e) => setGroup(e.target.value)}>      
+            <option>{editComponentObj.assignedToGroup}</option>     
             {
                 existingGroups.map(group => {
+                    if(group.groupName != editComponentObj.assignedToGroup){
                     return (
                         <option key={group._id} value={group.groupName}>{group.groupName}</option>
                     )
+                    }
                 })
             }
             </select>
